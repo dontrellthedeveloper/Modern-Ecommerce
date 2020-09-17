ed6c343fb83f15f616f3723da2f9099e9e459f2e
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2020-09-16 20:43:07 -0700

    added beginning of mainslider component for homepage

--- src/pages/HomeSection.js
+++ src/pages/HomeSection.js
@@ -14,16 +14,17 @@ import ReactVideoPlayer from '../components/videoplayer/videoplayer.component';
 import Slider from '../components/slider/slides.component';
 import Services from '../components/servicessection/services.component';
 import HomepageSlider from '../components/homepageslider/homepageslider.component';
+import MainSlider from '../components/mainslider/mainslider.component';
 
 const HomeSection = () => {
     return (
         <>
-        <InfoSection {...homeObjOne}/>
-        {/*
-        <HomepageSlider/>
-        */}    
-        <HomePage/>
+
+        <MainSlider/>
+
         
+        <HomePage/>
+
         <ReactVideoPlayer/>
         <Services/>
         <Jumbotron/>
