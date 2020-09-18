deb72fda292f8ff70eff9a67fddecc9df6fa168b
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2020-09-17 18:56:11 -0700

    added ability to store user data in application from firebase

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import ShopPage from './pages/shop/shop.component';
 
 import './App.css';
 import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import {auth} from './firebase/firebase.utils';
+import {auth, createUserProfileDocument} from './firebase/firebase.utils';
 
 
 class App extends Component {
@@ -26,8 +26,20 @@ class App extends Component {
       unsubscribeFromAuth = null;
     
       componentDidMount() {
-        this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-          this.setState({currentUser: user})
+        this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+          if (userAuth) {
+            const userRef = await createUserProfileDocument(userAuth)
+    
+            userRef.onSnapshot(snapShot => {
+              this.setState({
+                currentUser: {
+                  id: snapShot.id,
+                  ...snapShot.data()
+                }
+              })
+            });
+          }
+          this.setState({currentUser: userAuth})
         });
       }
     
