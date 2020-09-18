8c36d8931368ce68856bf82ba3750dafe4afa656
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2020-09-17 19:37:13 -0700

    implemented firebase utils, included ability to store authenticated users into our firestore database

--- src/pages/HomeSection.js
+++ src/pages/HomeSection.js
@@ -2,18 +2,20 @@ import React from 'react';
 import {
     homeObjOne,
     homeObjTwo,
-    homeObjThree,
-    homeObjFour
+    homeObjThree
+    // homeObjFour
 } from './Data';
 import { InfoSection } from '../components/Utils';
 
+
+// import Cards from "../components/cards/cards.component";
+// import Slider from '../components/slider/slides.component';
+// import HomepageSlider from '../components/homepageslider/homepageslider.component';
+
 import {Jumbotron} from '../components/jumbotron/jumbotron.component';
-import Cards from "../components/cards/cards.component";
 import HomePage from '../components/homepage/homepage.component';
 import ReactVideoPlayer from '../components/videoplayer/videoplayer.component';
-import Slider from '../components/slider/slides.component';
 import Services from '../components/servicessection/services.component';
-import HomepageSlider from '../components/homepageslider/homepageslider.component';
 import MainSlider from '../components/mainslider/mainslider.component';
 import ProductBanner from '../components/productbanner/productbanner.component';
 import ProductBanner2 from '../components/productbanner2/productbanner2.component';
